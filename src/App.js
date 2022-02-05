2dc3c07f97fe5c5c95727c4ccb2083c5f73b46b1
okkyprasetian <okkyprasetian@gmail.com>
2022-02-05 09:46:38 +0700

    write almost all content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,50 @@ function App() {
   return (
     <div className="App">
 
+      <div className="home">
+        <div className="home-content">
+          <div className="brand">
+            Trigonalice
+          </div>
+          <h1 className="home-h1"> Money Management at Your Fingertips </h1>
+          <p className="home-p">
+            Trigonalice will help you manage your finance
+          </p>
+        </div>
+        <div className="home-illust">
+          Image
+        </div>
+      </div>
+
+      <div className="feature">
+        <h1 className="feature-h1"> App Feature </h1>
+        <p className="feature-p">
+          We have three main feature in trigonalice. The most functionality you need for controlling your finance
+        </p>
+        <div className="cards"></div>
+      </div>
+
+      <div className="beta">
+
+        <h1 className="beta-h1">We Make Our Beta App Avaible for Everyone</h1>
+        <p className="beta-p">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium deserunt illo assumenda similique, exercitationem harum aspernatur commodi voluptatem et inventore ullam. Odio in accusamus fugiat reiciendis aliquid, itaque sed hic exercitationem placeat nihil laborum ut, quas beatae animi voluptatibus? Accusantium doloremque, nihil laboriosam sint odio deleniti quos doloribus iste facere.
+        </p>
+        <div className="beta-illust">
+          Image
+        </div>
+
+        <h2 className="beta-h2">Download Trigonalice Beta Version to Experience New Features</h2>
+        <p className="beta-download-p">
+          click button and install!
+        </p>
+
+        <div className="footer">
+          C2022. Trigonalice. All right reserved.
+        </div>
+
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Download.jsx
@@ -0,0 +1,9 @@
+function Download() {
+    return (
+        <button className="download">
+            Download
+        </button>
+    );
+}
+
+export default Download;
